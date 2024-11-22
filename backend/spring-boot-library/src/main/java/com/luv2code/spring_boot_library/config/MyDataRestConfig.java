e014baf48a41aa5a678b6ee377554e20f6f85932
amircherif-ac <amircherif-ac@hotmail.fr>
2024-11-22 17:15:19 +0100

    Search Books Component with Pagination - Frontend

---
+++ backend/spring-boot-library/src/main/java/com/luv2code/spring_boot_library/config/MyDataRestConfig.java
@@ -0,0 +1,4 @@
+package com.luv2code.spring_boot_library.config;
+
+public class MyDataRestConfig {
+}
---
+++ backend/spring-boot-library/src/main/java/com/luv2code/spring_boot_library/dao/BookRepository.java
@@ -0,0 +1,4 @@
+package com.luv2code.spring_boot_library.dao;
+
+public interface BookRepository {
+}
