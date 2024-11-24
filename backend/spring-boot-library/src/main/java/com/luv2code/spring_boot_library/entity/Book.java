6c0a40cb38333db5d3f1867ff1c7b36239bf5e7f
amircherif-ac <amircherif-ac@hotmail.fr>
2024-11-24 15:45:42 +0100

    Find by Title and filter by category - Backend

--- backend/spring-boot-library/src/main/java/com/luv2code/spring_boot_library/entity/Book.java
+++ backend/spring-boot-library/src/main/java/com/luv2code/spring_boot_library/entity/Book.java
@@ -1,4 +1,37 @@
 package com.luv2code.spring_boot_library.entity;
 
+import jakarta.persistence.*;
+import lombok.Data;
+
+@Entity
+@Table(name = "book")
+@Data
 public class Book {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private Long id;
+
+    @Column(name = "title")
+    private String title;
+
+    @Column(name = "author")
+    private String author;
+
+    @Column(name = "description")
+    private String description;
+
+    @Column(name = "copies")
+    private int copies;
+
+    @Column(name = "copiesAvailable")
+    private int copies_available;
+
+    @Column(name = "category")
+    private String category;
+
+    @Column(name = "img")
+    private String img;
+
 }
